zmeditation/django-oscar

commit ef3ccf08a707ae1250cdb8d5f2dc6f721d020dc4

Use new error-block class for error messages
At the moment, error messages get the class help-block. However the same
help-block class is applied to help text. It makes it hard to style
errors and help messages differently.

Work done by @timjacobi in #998. I was skeptical of all the CSS changes
and hence re-did the CSS compilation. I also only pulled across the
class changes and ignored some of the other HTML changes as they've
since been applied on master.

Fixes #998

Conflicts:
	oscar/templates/oscar/basket/partials/basket_content.html
	oscar/templates/oscar/dashboard/catalogue/product_update.html

oscar/templates/oscar/dashboard/catalogue/product_update.html
@@ -116,7 +116,7 @@
                                                 <h3>{% trans "Categories" %}</h3>
                                             </div>
                                             <table class="table table-striped table-bordered">
-                                                {% if product.categories.all %}
+                                                {% if product.categories %}
                                                     {% for category in product.categories.all %}
                                                         <tr><td>{{ category.full_name }}</td></tr>
                                                     {% endfor %}
@@ -153,7 +153,7 @@
                             </div>
                             <div class="well product-details">
                                 {% block product_details_content %}
-                                    <span class="help-block">{{ form.non_field_errors }}</span>
+                                    <span class="error-block">{{ form.non_field_errors }}</span>
                                         {% for field in form.hidden_fields %}
                                             {{ field }}
                                         {% endfor %}
@@ -253,7 +253,7 @@
                                                     {% else %}
                                                         {{ field }}
                                                         {% for error in field.errors %}
-                                                            <span class="help-block">
+                                                            <span class="error-block">
                                                                 {{ error }}
                                                             </span>
                                                         {% endfor %}
@@ -337,7 +337,7 @@
                                             <tr>
                                                 <td colspan="10">
                                                     {% for error in stockrecord_form.non_field_errors  %}
-                                                        <span class="help-block errorlist"><i class="icon-exclamation-sign"></i> {{ error }}</span>
+                                                        <span class="error-block errorlist"><i class="icon-exclamation-sign"></i> {{ error }}</span>
                                                     {% endfor %}
                                                 </td>
                                             </tr>

oscar/static/oscar/css/dashboard.css
@@ -2872,7 +2872,8 @@ td .btn-toolbar {
 .form-inline {
   margin-bottom: 0;
 }
-.form-inline .help-block:first-child {
+.form-inline .help-block:first-child,
+.form-inline .error-block:first-child {
   display: none;
 }
 .form-inline label {
@@ -2946,11 +2947,13 @@ input[type=text].hasDatepicker,
   padding: 0 0 5px 0;
   list-style: none;
 }
-.form-inline .help-block {
+.form-inline .help-block,
+.form-inline .error-block {
   display: inline-block;
   margin: 0 20px 0 10px;
 }
-.form-inline .help-block li {
+.form-inline .help-block li,
+.form-inline .error-block li {
   list-style: none;
   color: #B94A48;
 }

oscar/static/oscar/css/styles.css
@@ -1399,10 +1399,12 @@ select:focus:invalid:focus {
 .form-actions:after {
   clear: both;
 }
+.error-block,
 .help-block,
 .help-inline {
   color: #595959;
 }
+.error-block,
 .help-block {
   display: block;
   margin-bottom: 10px;
@@ -1692,6 +1694,7 @@ legend + .control-group {
 .form-horizontal .controls:first-child {
   *padding-left: 180px;
 }
+.form-horizontal .error-block,
 .form-horizontal .help-block {
   margin-bottom: 0;
 }
@@ -1700,7 +1703,13 @@ legend + .control-group {
 .form-horizontal textarea + .help-block,
 .form-horizontal .uneditable-input + .help-block,
 .form-horizontal .input-prepend + .help-block,
-.form-horizontal .input-append + .help-block {
+.form-horizontal .input-append + .help-block,
+.form-horizontal input .error-block,
+.form-horizontal select .error-block,
+.form-horizontal textarea .error-block,
+.form-horizontal .uneditable-input .error-block,
+.form-horizontal .input-prepend .error-block,
+.form-horizontal .input-append .error-block {
   margin-top: 10px;
 }
 .form-horizontal .form-actions {

oscar/static/oscar/less/bootstrap/forms.less
@@ -397,11 +397,13 @@ select:focus:invalid {
 // HELP TEXT
 // ---------
 
+.error-block,
 .help-block,
 .help-inline {
   color: lighten(@textColor, 15%); // lighten the text some for contrast
 }
 
+.error-block,
 .help-block {
   display: block; // account for any element using help-block
   margin-bottom: @baseLineHeight / 2;
@@ -669,6 +671,7 @@ legend + .control-group {
     }
   }
   // Remove bottom margin on block level help text since that's accounted for on .control-group
+  .error-block,
   .help-block {
     margin-bottom: 0;
   }
@@ -679,7 +682,7 @@ legend + .control-group {
   .uneditable-input,
   .input-prepend,
   .input-append {
-    + .help-block {
+    + .help-block, .error-block {
       margin-top: @baseLineHeight / 2;
     }
   }

oscar/static/oscar/less/dashboard.less
@@ -100,7 +100,7 @@ td .btn-toolbar {
 }
 .form-inline {
   margin-bottom: 0;
-  .help-block:first-child {
+  .help-block:first-child, .error-block:first-child {
     display:none;
   }
   label {
@@ -188,7 +188,7 @@ input[type=text].hasDatepicker,
   }
 }
 .form-inline {
-  .help-block {
+  .help-block, .error-block {
     display:inline-block;
     margin:0 20px 0 10px;
     li {

oscar/static/oscar/less/dashboard/mixins.less
@@ -161,6 +161,7 @@
 .formFieldState(@textColor: #555, @borderColor: #ccc, @backgroundColor: #f5f5f5) {
   // Set the text color
   > label,
+  .error-block,
   .help-block,
   .help-inline {
     color: @textColor;

oscar/templates/oscar/basket/partials/basket_content.html
@@ -72,8 +72,9 @@
                                         </div>
                                         {% for field_errors in form.errors.values %}
                                             {% for error in field_errors %}
-                                                <span class="help-block"><i class="icon-exclamation-sign"></i> {{ error }}</span>
+                                                <span class="error-block"><i class="icon-exclamation-sign"></i> {{ error }}</span>
                                             {% endfor %}
+
                                         {% endfor %}
                                     </p>
                                 </div>

oscar/templates/oscar/checkout/gateway.html
@@ -37,7 +37,7 @@
                         {{ form.password }}
                         <small><a href="{% url 'password-reset' %}" tabindex="-1">{% trans "Get a password reminder" %}</a></small>
                         {% for error in form.password.errors %}
-                            <span class="help-block">{{ error }}</span>
+                            <span class="error-block">{{ error }}</span>
                         {% endfor %}
                     </div>
                 </div>

commit 41126dbd89149548796247e660aeee0fc55a2496

When affixing nav errors, skip empty error blocks
ef3ccf08a707ae1250cdb8d5f2dc6f721d020dc4 introduced a new error block
class. dashboard.js' logic to affix the little red exclamation marks in
the ProductUpdateCreateView then assumed that the forms are full of
errors, even if the error block is empty. Hence empty error blocks are
excluded now.

Also fixed a regression introduced with that commit showing the category
summary.

oscar/static/oscar/js/oscar/dashboard.js
@@ -50,7 +50,7 @@ var oscar = (function(o, $) {
 
             // Adds error icon if there are errors in the product update form
             $('[data-behaviour="affix-nav-errors"] .tab-pane').each(function(){
-              var productErrorListener = $(this).find('[class*="error"]').closest('.tab-pane').attr('id');
+              var productErrorListener = $(this).find('[class*="error"]:not(:empty)').closest('.tab-pane').attr('id');
               $('[data-spy="affix"] a[href="#' + productErrorListener + '"]').append('<i class="icon-info-sign pull-right"></i>');
             });
 

oscar/templates/oscar/dashboard/catalogue/product_update.html
@@ -116,13 +116,11 @@
                                                 <h3>{% trans "Categories" %}</h3>
                                             </div>
                                             <table class="table table-striped table-bordered">
-                                                {% if product.categories %}
-                                                    {% for category in product.categories.all %}
-                                                        <tr><td>{{ category.full_name }}</td></tr>
-                                                    {% endfor %}
-                                                {% else %}
+                                                {% for category in product.categories.all %}
+                                                    <tr><td>{{ category.full_name }}</td></tr>
+                                                {% empty %}
                                                     <tr><td>{% trans "No categories setup for this product" %}</td></tr>
-                                                {% endif %}
+                                                {% endfor %}
                                             </table>
                                         </div>
                                         {% endblock %}