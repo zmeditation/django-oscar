zmeditation/django-oscar

commit cca43aa4d1b41833fabfa8aaf432df28f8b1e498

Remove form from the basket line formset and bypass its validation if related model instance does not exist any more.

src/oscar/apps/basket/views.py
@@ -152,7 +152,7 @@ class BasketView(ModelFormSetView):
 
         for form in formset:
             if (hasattr(form, 'cleaned_data')
-                    and form.cleaned_data['save_for_later']):
+                    and getattr(form.cleaned_data, 'save_for_later', False)):
                 line = form.instance
                 if self.request.user.is_authenticated:
                     self.move_line_to_saved_basket(line)

tests/functional/test_basket.py
@@ -200,3 +200,81 @@ class SavedBasketTests(WebTestCase):
         # we can't add more than stock level into basket
         self.assertEqual(Basket.open.get(id=basket.id).lines.get(product=product).quantity, 1)
         self.assertRedirects(response, reverse('basket:summary'))
+
+
+class BasketFormSetTests(WebTestCase):
+    csrf_checks = False
+
+    def test_formset_with_removed_line(self):
+        products = [create_product() for i in range(3)]
+        basket = factories.create_basket(empty=True)
+        basket.owner = self.user
+        basket.save()
+
+        add_product(basket, product=products[0])
+        add_product(basket, product=products[1])
+        add_product(basket, product=products[2])
+        response = self.get(reverse('basket:summary'))
+        formset = response.context['formset']
+        self.assertEqual(len(formset.forms), 3)
+
+        basket.lines.filter(product=products[0]).delete()
+
+        management_form = formset.management_form
+        data = {
+            formset.add_prefix('INITIAL_FORMS'): management_form.initial['INITIAL_FORMS'],
+            formset.add_prefix('MAX_NUM_FORMS'): management_form.initial['MAX_NUM_FORMS'],
+            formset.add_prefix('TOTAL_FORMS'): management_form.initial['TOTAL_FORMS'],
+            'form-0-quantity': 1,
+            'form-0-id': formset.forms[0].instance.id,
+            'form-1-quantity': 2,
+            'form-1-id': formset.forms[1].instance.id,
+            'form-2-quantity': 2,
+            'form-2-id': formset.forms[2].instance.id,
+        }
+        response = self.post(reverse('basket:summary'), params=data)
+        self.assertEqual(response.status_code, 302)
+        formset = response.follow().context['formset']
+        self.assertEqual(len(formset.forms), 2)
+        self.assertEqual(len(formset.forms_with_instances), 2)
+        self.assertEqual(basket.lines.all()[0].quantity, 2)
+        self.assertEqual(basket.lines.all()[1].quantity, 2)
+
+    def test_invalid_formset_with_removed_line(self):
+        products = [create_product() for i in range(3)]
+        basket = factories.create_basket(empty=True)
+        basket.owner = self.user
+        basket.save()
+
+        add_product(basket, product=products[0])
+        add_product(basket, product=products[1])
+        add_product(basket, product=products[2])
+        response = self.get(reverse('basket:summary'))
+        formset = response.context['formset']
+        self.assertEqual(len(formset.forms), 3)
+
+        basket.lines.filter(product=products[0]).delete()
+
+        stockrecord = products[1].stockrecords.first()
+        stockrecord.num_in_stock = 0
+        stockrecord.save()
+
+        management_form = formset.management_form
+        data = {
+            formset.add_prefix('INITIAL_FORMS'): management_form.initial['INITIAL_FORMS'],
+            formset.add_prefix('MAX_NUM_FORMS'): management_form.initial['MAX_NUM_FORMS'],
+            formset.add_prefix('TOTAL_FORMS'): management_form.initial['TOTAL_FORMS'],
+            'form-0-quantity': 1,
+            'form-0-id': formset.forms[0].instance.id,
+            'form-1-quantity': 2,
+            'form-1-id': formset.forms[1].instance.id,
+            'form-2-quantity': 2,
+            'form-2-id': formset.forms[2].instance.id,
+        }
+        response = self.post(reverse('basket:summary'), params=data)
+        self.assertEqual(response.status_code, 200)
+        formset = response.context['formset']
+        self.assertEqual(len(formset.forms), 3)
+        self.assertEqual(len(formset.forms_with_instances), 2)
+        self.assertEqual(basket.lines.all()[0].quantity, 1)
+        self.assertEqual(basket.lines.all()[1].quantity, 1)

src/oscar/apps/basket/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.conf import settings
 from django.db.models import Sum
+from django.forms.utils import ErrorDict
 from django.utils.translation import gettext_lazy as _
 
 from oscar.core.loading import get_model
@@ -19,15 +20,31 @@ class BasketLineForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.instance.strategy = strategy
 
-        max_allowed_quantity = None
-        num_available = getattr(self.instance.purchase_info.availability, 'num_available', None)
-        basket_max_allowed_quantity = self.instance.basket.max_allowed_quantity()[0]
-        if all([num_available, basket_max_allowed_quantity]):
-            max_allowed_quantity = min(num_available, basket_max_allowed_quantity)
-        else:
-            max_allowed_quantity = num_available or basket_max_allowed_quantity
-        if max_allowed_quantity:
-            self.fields['quantity'].widget.attrs['max'] = max_allowed_quantity
+        # Evaluate max allowed quantity check only if line still exists, in
+        # order to avoid check run against missing instance -
+        # https://github.com/django-oscar/django-oscar/issues/2873.
+        if self.instance.id:
+            max_allowed_quantity = None
+            num_available = getattr(self.instance.purchase_info.availability, 'num_available', None)
+            basket_max_allowed_quantity = self.instance.basket.max_allowed_quantity()[0]
+            if all([num_available, basket_max_allowed_quantity]):
+                max_allowed_quantity = min(num_available, basket_max_allowed_quantity)
+            else:
+                max_allowed_quantity = num_available or basket_max_allowed_quantity
+            if max_allowed_quantity:
+                self.fields['quantity'].widget.attrs['max'] = max_allowed_quantity
+
+    def full_clean(self):
+        if not self.instance.id:
+            self.cleaned_data = {}
+            self._errors = ErrorDict()
+            return
+        return super().full_clean()
+
+    def has_changed(self):
+        if not self.instance.id:
+            return False
+        return super().has_changed()
 
     def clean_quantity(self):
         qty = self.cleaned_data['quantity']

src/oscar/apps/basket/formsets.py
@@ -1,4 +1,5 @@
 from django.forms.models import BaseModelFormSet, modelformset_factory
+from django.utils.functional import cached_property
 
 from oscar.core.loading import get_classes, get_model
 
@@ -23,8 +24,23 @@ class BaseBasketLineFormSet(BaseModelFormSet):
         """
         if super()._should_delete_form(form):
             return True
+
+        # If related form instance already removed, let's remove this form
+        # as well.
+        if not form.instance.id:
+            return True
         if self.can_delete and 'quantity' in form.cleaned_data:
             return form.cleaned_data['quantity'] == 0
+
+    @cached_property
+    def forms_with_instances(self):
+        return [f for f in self.forms if f.instance.id]
+
+    def __iter__(self):
+        """
+        Skip forms with removed lines when iterating through the formset.
+        """
+        return iter(self.forms_with_instances)
 
 
 BasketLineFormSet = modelformset_factory(

commit 19b48e4910e804eaea411973510ffdf601695b1b

Fix broken basket "save for later" functionality
Regression introduced in cca43aa4d1b41833fabfa8aaf432df28f8b1e498

src/oscar/apps/basket/views.py
@@ -156,7 +156,7 @@ class BasketView(ModelFormSetView):
 
         for form in formset:
             if (hasattr(form, 'cleaned_data')
-                    and getattr(form.cleaned_data, 'save_for_later', False)):
+                    and form.cleaned_data.get('save_for_later', False)):
                 line = form.instance
                 if self.request.user.is_authenticated:
                     self.move_line_to_saved_basket(line)

tests/functional/test_basket.py
@@ -145,6 +145,63 @@ class BasketReportTests(TestCase):
 class SavedBasketTests(WebTestCase):
     csrf_checks = False
 
+    def test_moving_to_saved_basket_creates_new(self):
+        self.user = factories.UserFactory()
+        product = factories.ProductFactory()
+        basket = factories.BasketFactory(owner=self.user)
+        basket.add_product(product)
+
+        response = self.get(reverse('basket:summary'))
+        formset = response.context['formset']
+        form = formset.forms[0]
+
+        data = {
+            formset.add_prefix('INITIAL_FORMS'): 1,
+            formset.add_prefix('TOTAL_FORMS'): 1,
+            formset.add_prefix('MIN_FORMS'): 0,
+            formset.add_prefix('MAX_NUM_FORMS'): 1,
+            form.add_prefix('id'): form.instance.pk,
+            form.add_prefix('quantity'): form.initial['quantity'],
+            form.add_prefix('save_for_later'): True,
+        }
+        response = self.post(reverse('basket:summary'), params=data)
+
+        self.assertRedirects(response, reverse('basket:summary'))
+        self.assertFalse(Basket.open.get(pk=basket.pk).lines.exists())
+        self.assertEqual(Basket.saved.get(owner=self.user).lines.get(
+            product=product).quantity, 1)
+
+    def test_moving_to_saved_basket_updates_existing(self):
+        self.user = factories.UserFactory()
+        product = factories.ProductFactory()
+
+        basket = factories.BasketFactory(owner=self.user)
+        basket.add_product(product)
+
+        saved_basket = factories.BasketFactory(owner=self.user,
+                                               status=Basket.SAVED)
+        saved_basket.add_product(product)
+
+        response = self.get(reverse('basket:summary'))
+        formset = response.context['formset']
+        form = formset.forms[0]
+
+        data = {
+            formset.add_prefix('INITIAL_FORMS'): 1,
+            formset.add_prefix('TOTAL_FORMS'): 1,
+            formset.add_prefix('MIN_FORMS'): 0,
+            formset.add_prefix('MAX_NUM_FORMS'): 1,
+            form.add_prefix('id'): form.instance.pk,
+            form.add_prefix('quantity'): form.initial['quantity'],
+            form.add_prefix('save_for_later'): True,
+        }
+        response = self.post(reverse('basket:summary'), params=data)
+
+        self.assertRedirects(response, reverse('basket:summary'))
+        self.assertFalse(Basket.open.get(pk=basket.pk).lines.exists())
+        self.assertEqual(Basket.saved.get(pk=saved_basket.pk).lines.get(
+            product=product).quantity, 2)
+
     def test_moving_from_saved_basket(self):
         self.user = User.objects.create_user(username='test', password='pass',
                                              email='test@example.com')